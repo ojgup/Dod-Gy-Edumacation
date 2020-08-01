0ceaac371e1f38a5b5d8d23d00fdfa1920c1d913
ojgup <102542177@Student.swin.edu.au>
2020-08-01 15:05:02 +1000

    Added the strikethrough style.

--- APP/src/app/app.module.ts
+++ APP/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavLinkComponent } from './nav-link/nav-link.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavLinkComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
