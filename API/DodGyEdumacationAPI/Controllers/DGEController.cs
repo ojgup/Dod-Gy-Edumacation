d7078d702fc93a750eaa6e5aeed36b6a87e2f9d4
Steve G <s.grouios@hotmail.com>
2020-08-06 15:39:22 +1000

    Session validation added to Session Start endpoint

--- API/DodGyEdumacationAPI/Controllers/DGEController.cs
+++ API/DodGyEdumacationAPI/Controllers/DGEController.cs
@@ -40,28 +40,32 @@ namespace DodGyEdumacationAPI.Controllers
 
 
         // POST: api/DGE/start
-        //Returns incremented sessionID from DB if INSERT accepted, otherwise error
+        //Returns incremented sessionID from DB if INSERT accepted, otherwise error and returns -1
         [HttpPost("start")]
         public async Task<int> SessionStart(Session session)
         {
-            var sessionId = new SqlParameter
+            if (!await _context.Session.AnyAsync(s => s.UserId == session.UserId && s.SessionEnd == null))
             {
-                ParameterName = "@SESSIONID",
-                DbType = System.Data.DbType.Int32,
-                Direction = System.Data.ParameterDirection.Output
+                var sessionId = new SqlParameter
+                {
+                    ParameterName = "@SESSIONID",
+                    DbType = System.Data.DbType.Int32,
+                    Direction = System.Data.ParameterDirection.Output
+                };
 
-            };
+                SqlParameter roomCode = new SqlParameter("@ROOMCODE", session.RoomCode);
+                SqlParameter sessionStart = new SqlParameter("@SESSIONSTART", session.SessionStart.ToString("yyyy-MM-dd HH:mm:ss.fff"));
+                SqlParameter sessionType = new SqlParameter("@SESSIONTYPE", session.SessionType);
+                SqlParameter userId = new SqlParameter("@USERID", session.UserId);
 
-            SqlParameter roomCode = new SqlParameter("@ROOMCODE", session.RoomCode);
-            SqlParameter sessionStart = new SqlParameter("@SESSIONSTART", session.SessionStart.ToString("yyyy-MM-dd HH:mm:ss.fff"));
-            SqlParameter sessionType = new SqlParameter("@SESSIONTYPE", session.SessionType);
-            SqlParameter userId = new SqlParameter("@USERID", session.UserId);
+                var sql = "EXEC @SESSIONID = START_SESSION @ROOMCODE, @SESSIONSTART, @SESSIONTYPE, @USERID";
 
-            var sql = "EXEC @SESSIONID = START_SESSION @ROOMCODE, @SESSIONSTART, @SESSIONTYPE, @USERID";
+                await _context.Database.ExecuteSqlRawAsync(sql, sessionId, roomCode, sessionStart, sessionType, userId);
 
-            await _context.Database.ExecuteSqlRawAsync(sql, sessionId, roomCode, sessionStart, sessionType, userId);
-
-            return Convert.ToInt32(sessionId.Value);
+                return Convert.ToInt32(sessionId.Value);
+            }
+            else
+                return -1;
         }
 
     }
