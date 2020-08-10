b1f577f704bb332e13502e75c48424cd36a5b5a0
MitchellHenry <48079181+MitchellHenry@users.noreply.github.com>
2020-08-10 14:29:46 +1000

    Added some styles and Added some button style functions

--- APP/src/app/button-box/button-box.component.ts
+++ APP/src/app/button-box/button-box.component.ts
@@ -9,7 +9,22 @@ export class ButtonBoxComponent implements OnInit {
 
   constructor() { }
 
+  ClassButtonClicked() {
+    let Classbtn = document.getElementById("ClassButton");
+    let OfficeBtn = document.getElementById("OfficeButton");
+    Classbtn.style.backgroundColor = "grey";
+    OfficeBtn.style.backgroundColor = "#3454D1";
+  }
+  OfficeButtonClicked(){
+    let Classbtn = document.getElementById("ClassButton");
+    let OfficeBtn = document.getElementById("OfficeButton");
+    Classbtn.style.backgroundColor = "#D1345B";
+    OfficeBtn.style.backgroundColor = "grey";
+  }
+
   ngOnInit(): void {
+    let Classbtn = document.getElementById("ClassButton");
+    Classbtn.style.backgroundColor = "grey";
   }
 
 }
