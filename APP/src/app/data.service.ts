94c2f61ca5e8d7c91499026536d0485cdc5779a6
Steve G <s.grouios@hotmail.com>
2020-08-21 15:05:09 +1000

    Implementing Report endpoint

--- APP/src/app/data.service.ts
+++ APP/src/app/data.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Session} from './session';
 import {User} from './user';
+import {Report} from './report';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  apiURL = "https://localhost:5001/api/DGE/start";
+  apiURL = "https://localhost:5001/api";
 
   constructor(private _http: HttpClient) { }
 
@@ -17,5 +18,10 @@ export class DataService {
 
   postSession(sessionEntered: Session){
     this._http.post("https://localhost:5001/api/DGE/start", sessionEntered).subscribe();
+  }
+
+  getReport(/*userId: string, start: Date, end: Date*/){
+    return this._http.get<Array<Report>>("https://localhost:5001/api/DGE/report?userId=103043778"
+    + "&start=2020-01-05T00:00:00&end=2020-01-09T00:00:00");
   }
 }
--- APP/src/app/reporting/reporting.component.ts
+++ APP/src/app/reporting/reporting.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
-import { Session } from '../session';
+import {Report} from '../report';
 
 @Component({
   selector: 'app-reporting',
@@ -12,11 +12,17 @@ export class ReportingComponent implements OnInit {
   userID: string;
   StartDate: Date;
   EndDate: Date;
+  reports: Array<Report>;
+
   constructor(public dataService: DataService) { }
 
   ngOnInit(): void {
   }
+  
   GetSessions(){
-
+    this.dataService.getReport().subscribe((data) => {
+      this.reports = data;
+      console.log(this.reports);
+    })
   }
 }
