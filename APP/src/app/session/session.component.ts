ec354fd65fa6c860d56b71ebfc040e0da0d6d4fb
JoeySuttonPreece <102568393@student.swin.edu.au>
2020-08-31 11:34:34 +1000

    switch back to old time validation

--- APP/src/app/session/session.component.ts
+++ APP/src/app/session/session.component.ts
@@ -15,11 +15,8 @@ export class SessionComponent implements OnInit {
 
   roomNumber: string;
 
-  hourEntered: number;
-  minutesEntered: number;
-
-  hourLeft: number;
-  minutesLeft: number;
+  hours: number;
+  minutes: number;
 
   sessionType: string = "Class";
 
@@ -35,6 +32,11 @@ export class SessionComponent implements OnInit {
     this.dataService.session.subscribe((value: Session) => {
       console.log(value);
       this.session = value;
+      this.hours = null;
+      this.minutes = null;
+      if (!value) {
+        this.roomNumber = null;
+      }
     });
   }
 
@@ -44,44 +46,82 @@ export class SessionComponent implements OnInit {
     }
   }
 
+  // sessionEntered() {
+  //   var date: Date = new Date();
+  //   date.setHours(this.hours, this.minutes, 0, 0);
+
+  //   var difference = new Date();
+  //   difference.setHours(difference.getHours() - 6);
+
+  //   //Close Session
+  //   if (this.session != null) {
+  //     date.setHours(date.getHours());
+  //     this.session.sessionEnd = date.toJSON();
+  //     console.log(this.session);
+  //     this.dataService.postEndSession(this.session);
+  //     this.roomNumber = "";
+  //     this.hours = null;
+  //     this.minutes = null;
+  //   }
+
+  //   //Start Session
+  //   if (date > difference) {
+  //     date.setHours(date.getHours());
+  //     if (this.session == null) {//Time acceptable
+  //       let enteredSession: Session =
+  //       {
+  //         'roomCode': this.roomNumber,
+  //         'sessionStart': date.toJSON(),
+  //         'userID': this.user.userId,
+  //         'sessionType': this.sessionType,
+  //       };
+
+  //       this.dataService.postStartSession(enteredSession);
+  //       this.hours = null;
+  //       this.minutes = null;
+  //       //Need to navigate to Home if successful session, otherwise display error
+  //     }
+  //   }
+  //   else
+  //     alert("You cannot enter a session time more than 6 hours before your current time");
+  // }
   sessionEntered() {
-    var date: Date = new Date();
-    date.setHours(this.hourEntered, this.minutesEntered, 0, 0);
+    let date: Date = new Date();
+    date.setHours(this.hours, this.minutes, 0, 0);
 
-    var difference = new Date();
-    difference.setHours(difference.getHours() - 6);
+    let now = new Date();
+    let diff: number = date.getTime() - now.getTime();
 
-    //Close Session
-    if (this.session != null) {
-      date.setHours(date.getHours());
+    if (now.getHours() < 6 && diff >= 18 * 60 * 60 * 1000) {
+      console.log('yesterday');
+      date.setTime(date.getTime() - 24 * 60 * 60 * 1000);
+      diff -= 24 * 60 * 60 * 1000;
+    }
+    if (diff < -6 * 60 * 60 * 1000) {
+      console.log('too long ago');
+      return;
+    }
+    else if (diff > 0) {
+      console.log('try logging it when you have left');
+      return;
+    }
+
+    if (this.session == null) { //start session
+      let enteredSession: Session =
+      {
+        'roomCode': this.roomNumber,
+        'sessionStart': date.toJSON(),
+        'userID': this.user.userId,
+        'sessionType': this.sessionType,
+      };
+
+      this.dataService.postStartSession(enteredSession);
+    }
+    else { //end session
       this.session.sessionEnd = date.toJSON();
       console.log(this.session);
       this.dataService.postEndSession(this.session);
-      this.roomNumber = "";
-      this.hourEntered = null;
-      this.minutesEntered = null;
     }
-
-    //Start Session
-    if (date > difference) {
-      date.setHours(date.getHours());
-      if (this.session == null) {//Time acceptable
-        let enteredSession: Session =
-        {
-          'roomCode': this.roomNumber,
-          'sessionStart': date.toJSON(),
-          'userID': this.user.userId,
-          'sessionType': this.sessionType,
-        };
-
-        this.dataService.postStartSession(enteredSession);
-        this.hourEntered = null;
-        this.minutesEntered = null;
-        //Need to navigate to Home if successful session, otherwise display error
-      }
-    }
-    else
-      alert("You cannot enter a session time more than 6 hours before your current time");
   }
 
   ClassButtonClicked() {
